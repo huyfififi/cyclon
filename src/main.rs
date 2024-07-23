84f367bca2024b469a732bb7af8eda4f977aa504
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-07-22 20:21:55 -0700

    Add the basic structure of the command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "Cyclon")]
+#[command(version = "0.0.1")]
+#[command(author = "Kazuki Kijima <kazukiyoshida96@gmail.com>")]
+#[command(about = "Check cyclomatic complexity")]
+struct Args {
+    #[arg(index(1))]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("input path: {}", args.path);
 }
